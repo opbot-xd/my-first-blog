86317e6a8aad5abfcd36be2a555f804bc455a873
Krishna Awasthi <awasthikrishna23052005@gmail.com>
2024-12-05 13:55:45 +0530

    refactor: added doc string

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    """Represents a blog post."""
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -11,8 +12,10 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
+        """Publishes the post by setting the published date and saving it."""
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
+        """Returns a string representation of the post (its title)."""
         return self.title
